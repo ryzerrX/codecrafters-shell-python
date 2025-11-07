9d607a6ce09d13a80f3f4d33f41f71019a362757
ryzerrX <toshj2005@gmail.com>
2025-11-07 13:18:02 +0530

    Implemented exit

--- app/main.py
+++ app/main.py
@@ -2,14 +2,18 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    pass
-    error_msg()
-    main()
+    while True:
+        # TODO: Uncomment the code below to pass the first stage
+        sys.stdout.write("$ ")
+        pass
+        user_command = input()
+        if user_command == "exit 0" or user_command == "exit 1":
+            break
+        else:
+            error_msg(user_command)
 
-def error_msg():
-    user_command = input()
+
+def error_msg(user_command):
     print(f"{user_command}: command not found")
 
 if __name__ == "__main__":
