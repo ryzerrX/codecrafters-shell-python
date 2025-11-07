3692d24c05efdd4019507ca775ade28b76ba9a11
ryzerrX <toshj2005@gmail.com>
2025-11-07 12:29:48 +0530

    Fix test failure

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
     pass
 
     user_command = input()
-    print(f"{user_command} : command not found")
+    print(f"{user_command}: command not found")
 
 if __name__ == "__main__":
     main()
