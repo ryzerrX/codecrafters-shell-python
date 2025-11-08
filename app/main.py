de75bfa3dce91270febbab36d6f19b7c9600f8e8
ryzerrX <toshj2005@gmail.com>
2025-11-08 11:45:04 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,20 +1,61 @@
 import sys
 
+def handle_echo(arguments):
+    print(" ".join(arguments))
+
+def handle_exit(arguments):
+    if '0' in arguments:
+        print("Exiting with status 0 (success)")
+        sys.exit(0)
+    elif '1' in arguments:
+        print("Exiting with status 1 (error)")
+        sys.exit(1)
+    else:
+        print("invalid argument")
+
+def error_msg(command_name):
+    print(f"{command_name}: command not found")
+
+COMMAND_MAP = {
+    "echo": handle_echo,
+    "exit": handle_exit
+}
 
 def main():
     while True:
         # TODO: Uncomment the code below to pass the first stage
         sys.stdout.write("$ ")
         pass
+
+        # take user i/p
         user_command = input()
-        if user_command == "exit 0" or user_command == "exit 1":
-            break
+
+        # Skip empty input
+        if not user_command:
+            continue
+
+        # Splitting the Input into a list
+        parts = user_command.split()
+        command_name = parts[0]
+        arguments = parts[1:]
+
+        # Finding the Command Function
+        command_function = COMMAND_MAP.get(command_name)
+
+        if command_function:
+            command_function(arguments)
         else:
-            error_msg(user_command)
+            error_msg(command_name)
 
+        # if user_command == "exit 0" or user_command == "exit 1":
+        #     break
+        # elif user_command[:5] == "echo ":
+        #     cmd_echo(user_command)
+        # else:
+        #     error_msg(user_command)
 
-def error_msg(user_command):
-    print(f"{user_command}: command not found")
+# def cmd_echo(user_command):
+#     print(user_command[5:])
 
 if __name__ == "__main__":
     main()
