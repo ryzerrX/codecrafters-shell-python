35a1afe0fd89f0f3a715af33af423553679de1ab
ryzerrX <toshj2005@gmail.com>
2025-11-08 13:00:29 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 def handle_echo(arguments):
     print(" ".join(arguments))
@@ -14,11 +15,40 @@ def handle_exit(arguments):
         print("invalid argument")
 
 def handle_type(arguments):
-    for args in arguments:
-        if args in COMMAND_MAP:
-            print(f"{args} is a shell builtin")
-        else:
-            print(f"{args}: not found")
+        # If no argument was given
+        if not arguments:
+            return
+        
+        arg = arguments[0]
+
+        # Checking for builtin command
+        if arg in COMMAND_MAP:
+            print(f"{arg} is a shell builtin")
+            return
+        
+        # Get PATH 
+        path_string = os.environ.get("PATH")
+
+        # If PATH is not given
+        if not path_string:
+            print(f"{arg}: not found")
+            return
+        
+        directories = path_string.split(os.pathsep) # Forming a list of possible directories
+
+        # Searching the PATH directories to check is the file exists and is executable
+        found = False
+        for directory in directories:
+            full_path = os.path.join(directory, arg)
+
+            if os.path.exists(full_path) and os.access(full_path, os.X_OK):
+                print(f"{arg} is {full_path}")
+                found = True
+                break
+        
+        # If the file is still not found
+        if not found:
+            print(f"{arg}: not found")
 
 def error_msg(command_name):
     print(f"{command_name}: command not found")
